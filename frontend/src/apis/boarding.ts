55d4585f213b0943bfad1391bcf937db7bade95c
mercuryedge117 <mercucyedge@gmail.com>
2026-02-12 13:59:11 -0600

    apis cleanup & fix on profile & boarding

--- frontend/src/apis/boarding.ts
+++ frontend/src/apis/boarding.ts
@@ -5,36 +5,18 @@ const API_URL = '/boarding';
 const API_URL_ADMIN = '/boardings';
 
 export const submitApplication = async (data: IProfileFull) => {
-    try {
-        const response = await api.post(`${API_URL}/me`, data);
-        return response.data.data;
-    } catch (error) {
-        console.error('Error submit boarding application:', error);
-        throw error;
-    }
-
+    const response = await api.post(`${API_URL}/me`, data);
+    return response.data.data;
 }
 
 export const reSubmitApplication = async (data: IProfileFull) => {
-    try {
-        const response = await api.put(`${API_URL}/me`, data);
-        return response.data.data;
-    } catch (error) {
-        console.error('Error re-submit boarding application:', error);
-        throw error;
-    }
-
+    const response = await api.put(`${API_URL}/me`, data);
+    return response.data.data;
 }
 
 export const getApplicationStatus = async () => {
-    try {
-        const response = await api.get(`${API_URL}/me`);
-        return response.data.data;
-    } catch (error) {
-        console.error('Error fetching boarding status:', error);
-        throw error;
-    }
-
+    const response = await api.get(`${API_URL}/me`);
+    return response.data.data;
 }
 
 export type BoardingUnion = "PENDING" | "REJECTED" | "APPROVED"
--- frontend/src/apis/profile.ts
+++ frontend/src/apis/profile.ts
@@ -5,43 +5,23 @@ const API_URL = '/profile';
 const API_URL_ADMIN = '/profiles';
 
 export const getOwnProfile = async () => {
-    try {
-        const response = await api.get(`${API_URL}/me`);
-        return response.data.data;
-    } catch (error) {
-        console.error('Error fetching profile of currentUser:', error);
-        throw error;
-    }
+    const response = await api.get(`${API_URL}/me`);
+    return response.data.data;
 }
 
 export const updateOwnProfile = async (payload: IProfile) => {
-    try {
-        const response = await api.put(`${API_URL}/me`, payload);
-        return response.data.data;
-    } catch (error) {
-        console.error('Error updating profile of currentUser:', error);
-        throw error;
-    }
+    const response = await api.put(`${API_URL}/me`, payload);
+    return response.data.data;
 }
 
 
 export const fetchAllProfiles = async () => {
-    try {
-        const response = await api.get(`${API_URL_ADMIN}`);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching profiles:', error);
-        throw error;
-    }
+    const response = await api.get(`${API_URL_ADMIN}`);
+    return response.data.data;
 }
 
 
 export const getProfileById = async (id: string) => {
-    try {
-        const response = await api.get(`${API_URL_ADMIN}/${id}`);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching employee profile:', error);
-        throw error;
-    }
+    const response = await api.get(`${API_URL_ADMIN}/${id}`);
+    return response.data.data;
 }
