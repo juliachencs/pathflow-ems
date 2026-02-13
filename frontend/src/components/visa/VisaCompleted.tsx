81acd8be50b52bf731a4ca7f4c5f821f59342c23
mercuryedge117 <mercucyedge@gmail.com>
2026-02-12 18:18:07 -0600

    minor fix #3

--- frontend/src/components/visa/VisaCompleted.tsx
+++ frontend/src/components/visa/VisaCompleted.tsx
@@ -1,28 +1,49 @@
 import { SmileOutlined } from "@ant-design/icons";
 import { Divider, Result } from "antd";
-import UploadedDocs from "../profile/components/UploadedDocs";
+import UploadedDocs, {
+  type UploadedDoc,
+} from "../profile/components/UploadedDocs";
 import type { VisaDocuments } from "../../app/types";
 
 interface VisaCompletedProps {
-  docPack?: VisaDocuments;
+  documents?: VisaDocuments;
 }
 
-const VisaCompleted: React.FC<VisaCompletedProps> = ({ docPack }) => {
+const VisaCompleted: React.FC<VisaCompletedProps> = ({ documents }) => {
+  let docPack: UploadedDoc[] = [];
+  if (documents) {
+    docPack = [
+      {
+        label: "OPT Receipt",
+        url: documents.OPT,
+      },
+      {
+        label: "EAD Card",
+        url: documents.EAD,
+      },
+      {
+        label: "I-983 Form",
+        url: documents.I983,
+      },
+      {
+        label: "I-20 Form",
+        url: documents.I20,
+      },
+    ];
+  }
+
   return (
     <>
       <Result
         icon={<SmileOutlined />}
         title="Great, you have completed all the operations!"
-        subTitle='Please contact HR if you have any question about your visa status or update'
+        subTitle="Please contact HR if you have any question about your visa status or update"
       ></Result>
 
       {docPack && (
         <>
           <Divider></Divider>
-          <UploadedDocs
-            docPack={{ visaDocuments: docPack }}
-            title="Uploaded Visa Documents"
-          />
+          <UploadedDocs docPack={docPack} title="Uploaded Visa Documents" />
         </>
       )}
     </>
